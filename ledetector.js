1e252e4fe4fc61681dfe240fe239310bdc1b815f
Coop <73203995+coopeeo@users.noreply.github.com>
2024-02-10 17:37:44 -0600

    eeee

--- ledetector.js
+++ ledetector.js
@@ -5,7 +5,7 @@ import { pipeline } from 'stream/promises';
 import { Extract } from 'unzip-stream';
 import got from 'got';
 import { setTimeout } from 'timers/promises';
-import { rcompare, valid } from 'semver';
+import { compare, rcompare, valid } from 'semver';
 import { marked } from 'marked';
 import { htmlEscape } from 'escape-goat';
 
@@ -146,6 +146,17 @@ const modscompare = readdirSync('_mods2_tmp', { withFileTypes: true })
         return (geodeVer.startsWith('2.') || geodeVer.startsWith('v2.')) && !!latestVer.modJSON.gd;
     });
 
+modsBar.stop()
+modsBar.start(100, 0, { status: 'Parsing mods from Coop\'s repo into object instead of array' });
+var modscomparereel = {}
+for (let index = 0; index < modscompare.length; index++) {
+    const element = modscompare[index];
+    modscomparereel[element.id] = element
+    modsBar.update(index / modscompare.length * 100);
+}
+modsBar.update(100, { status: 'Mods parsed' });
+modsBar.stop();
+
 const genBar = new SingleBar({
     format: '{bar} {percentage}% {status}'
 }, Presets.shades_classic);
@@ -207,106 +218,15 @@ function developersTextOnListing(developers) {
 var lenumberofmod=-1
 for (const mod of mods) {
     lenumberofmod+=1
-    var modCurrentJson = mod.versions[0].modJSON
-    var modDevelopers = modCurrentJson.developers ?? [modCurrentJson.developer];
+    var modVersion = mod.versions[0].version
+    // Compare mod to see if it updated with coop's repo
+    if (modVersion == modscomparereel[mod.id].versions[0].version)
+    
 
-    searchPageContent.push(html`
-        <article
-            class="mod-card"
-            data-name="${modCurrentJson.name}"
-            data-developer="${developersTextOnListing(modDevelopers)}"
-            data-description="${modCurrentJson.description}"
-            data-about="${mod.about.replace(/\"/g, '')}"
-            data-tags="${mod.versions[0].entryJSON.tags?.join('') ?? ''}"
-            data-default-score=${mods.length - searchPageContent.length}
-        >
-            <div class="info">
-                <div class="img"><img src="${mod.logoURL}"></div>
-                <h1>${modCurrentJson.name}</h1>
-                <h3><i class="author">${developersTextOnListing(modDevelopers)}</i> • <i class="version">${mod.versions[0].version}</i></h3>
-                <p class="short-desc">${cutText(modCurrentJson.description)}</p>
-            </div>
-            <div class="buttons">
-                <a href="./${pathEscape(mod.id)}">View</a>
-            </div>
-        </article>
-    `);
-
-    mkdirSync(filepath`gen/mods/${mod.id}`);
-    writeFileSync(filepath`gen/mods/${mod.id}/index.html`,
-        modPageTemplate
-            .replace(/\$MOD_ID/g, escape(mod.id))
-            .replace(/\$MOD_NAME/g, escape(modCurrentJson.name))
-            .replace(/\$MOD_VERSION_LINKS/g, mod.versions
-                .map(ver => html`<a
-                    href="#"
-                    fake-download="${ver.entryJSON.mod.download}"
-                    class="
-                        button wide
-                        border-solid border-2 border-light border-opacity-25 hover:border-cyan
-                        bg-opacity-0 hover:bg-opacity-0
-                        px-10
-                    "
-                ><i data-feather="download"></i>${ver.version}</a>`)
-                .join('')
-            )
-            .replace(/\$MOD_VERSION/g, escape(mod.versions[0].version))
-            .replace(/\$MOD_DEVELOPER/g, escape(modDevelopers.join(" & ")))
-            .replace(/\$MOD_ICON_URL/g, escape(mod.logoURL))
-            .replace(/\$MOD_DOWNLOAD_URL/g, escape(mod.versions[0].entryJSON.mod.download))
-            .replace(/\$MOD_ABOUT_MD/g, marked(mod.about))
-            .replace(/\$MOD_LINKS/g, withIfEmpty(Object.entries({
-                'repository': 'github',
-            })
-                .filter(link => link[0] in modCurrentJson)
-                .map(link => html`
-                    <a
-                        class="button wide border-solid border-2 border-gray-light"
-                        href="${modCurrentJson[link[0]]}"
-                    >
-                        <i data-feather="${link[1]}"></i>
-                        Repository
-                    </a>
-                `), html`
-                    <a
-                        class="emptylink wide border-solid border-2 border-gray-light"
-                    >
-                        No links available
-                    </a>
-                `)
-                .join('')
-            )
-            .replace(/\$MOD_TAGS/g, withIfEmpty(mod.versions[0].entryJSON.tags.map(tag => html`
-                <span class="mod-tag">${tag}</span>
-            `), '<span class="mod-tag mod-tag-none">None</span>').join(''))
-            .replace(/\$MOD_AVAILABLE_PLATFORMS/g, Object.entries({
-                'windows':   'media/windows.svg',
-                'macos':     'media/apple.svg',
-                'android64': 'media/android.svg',
-                'ios':       'media/ios.svg',
-            })
-                .filter(plat => mod.versions[0].entryJSON.platforms.includes(plat[0]))
-                .map(plat => readFileSync(plat[1]).toString())
-                .join('')
-            )
-            .replace(/\$MOD_UNAVAILABLE_PLATFORMS/g, Object.entries({
-                'windows':   'media/windows.svg',
-                'macos':     'media/apple.svg',
-                'android64': 'media/android.svg',
-                'ios':       'media/ios.svg',
-            })
-                .filter(plat => !mod.versions[0].entryJSON.platforms.includes(plat[0]))
-                .map(plat => readFileSync(plat[1]).toString())
-                .join('')
-            )
-    );
+    
     genBar.update(searchPageContent.length / mods.length * 99);
 }
 
 genBar.update(99, { status: 'Writing search page' });
-writeFileSync('gen/mods/index.html',
-    readFileSync('src/mods/index.html').toString()
-        .replace(/\$INSERT_MODS_CONTENT_HERE/g, searchPageContent.join(''))
-);
 genBar.update(100, { status: 'Pages finished' });
 genBar.stop();
